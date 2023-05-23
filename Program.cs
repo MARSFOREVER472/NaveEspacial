f79e7dabf73c10c31108c06e314475c01dc3fd37
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-05-23 13:22:17 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+using NaveEspacial;
+
+Ventana ventana = new Ventana(45,20);
+
+Console.WriteLine("Ancho Máximo:" +Console.LargestWindowWidth);
+Console.WriteLine("Altura Máxima:" + Console.LargestWindowHeight);
+
+Console.SetCursorPosition(5, 5);
+Console.Write("Estamos trabajando para solucionarlo");
+
+Console.SetCursorPosition(5, 8);
+Console.Write("Vuelve a intentarlo más tarde");
+
+Console.ReadKey();
---
+++ Ventana.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaveEspacial
+{
+    internal class Ventana
+    {
+        public int Ancho { get; set; }
+        public int Altura { get; set; }
+        public Ventana(int ancho, int altura)
+        {
+            Ancho = ancho;
+            Altura = altura;
+            Init();
+
+        }
+
+        private void Init()
+        {
+            Console.SetWindowSize(Ancho,Altura);
+            Console.Title = "Nave";
+        }
+    }
+}

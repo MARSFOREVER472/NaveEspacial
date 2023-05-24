b349a52a39507ebea699081be75f8920d639bbff
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-05-24 13:25:29 -0400

    added a new background frame for the game

---
+++ Project/Program.cs
@@ -0,0 +1,6 @@
+﻿using NaveEspacial.Project;
+using System.Drawing;
+
+Ventana ventana = new Ventana(150,45,ConsoleColor.Black,new Point(1,1),new Point(100,25));
+ventana.DibujarMarco();
+Console.ReadKey();

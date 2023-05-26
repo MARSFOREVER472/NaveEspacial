5f895c520798330ae937e900c064d580d8b19a27
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-05-26 18:11:31 -0400

    a new element was added in the ship class inside the console.

---
+++ Project/Nave.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace NaveEspacial.Project
+{
+    internal class Nave
+    {
+        public float Vida { get; set; }
+        public Point Posicion { get; set; }
+        public ConsoleColor Color { get; set; }
+        public Ventana VentanaC { get; set; }
+
+        public Nave(Point posicion, ConsoleColor color, Ventana ventana)
+        {
+            Posicion = posicion;
+            Color = color;
+            VentanaC = ventana;
+            Vida = 100;
+        }
+
+        public void Dibujar()
+        {
+            Console.ForegroundColor = Color;
+            int x = Posicion.X;
+            int y = Posicion.Y;
+
+            Console.SetCursorPosition(x + 3, y);
+            Console.Write("A");
+            Console.SetCursorPosition(x + 1, y + 1);
+            Console.Write("<{x}>");
+            Console.SetCursorPosition(x, y + 2);
+            Console.Write("± W W ±");
+        }
+    }
+}
--- Project/Program.cs
+++ Project/Program.cs
@@ -1,6 +1,9 @@
 ﻿using NaveEspacial.Project;
 using System.Drawing;
 
-Ventana ventana = new Ventana(170,65,ConsoleColor.Black,new Point(1,1),new Point(115,45));
+Ventana ventana = new Ventana(170, 65, ConsoleColor.Black, new Point(1, 1), new Point(115, 45));
 ventana.DibujarMarco();
+Nave nave = new Nave(new Point(55, 25), ConsoleColor.Yellow, ventana);
+nave.Dibujar();
+
 Console.ReadKey();
